cbc7f1db9d285640aaf30ed4b7c01ab5c72c1959
root <zazouimmo@gmail.com>
2021-06-29 19:35:52 +0200

    ajout des fonction et d'un main pour testez le applybackup

--- applybackup.py
+++ applybackup.py
@@ -13,48 +13,48 @@ from datetime import timedelta
 
 
 local_path = '/home/localuser/Saves/'
-sql = 'dump.sql' #keep
+sql = 'dump.sql'
 save_path = 'var/www/backupsql/'
 bdd_user = 'wordpressuser'
 bdd_pass = 'toto'
 bdd = 'wordpress'
 
 
-#extract
-try:
-	with tarfile.open(local_path+"Sauvegardedu20210526.tar") as tar:   #remplacer le sauvegardedublablabla par le argv
-		tar.extractall(local_path)
-#		exit()
-except tarfile.ExtractError:
-	print ('erreur')
-
-
-#restoresql
-try:
-	k = subprocess.Popen(["mysql -u"+ bdd_user +" -p"+ bdd_pass +" "+ bdd +" < "+ local_path+save_path+sql], shell=True)
-except:
-	print('nope')
-
-#restorewordp
-original = '/home/localuser/Saves/var/www/html/'
-target = '/var/www/'
-
-for files in os.listdir(target):
-	i = os.path.join(target, files)
+def extract():
 	try:
-		shutil.rmtree(i)
-	except Exception as e:
-		print('cannot delete : ' + e)
-try:
-	shutil.move(original, target)
-except:
-	print('erreur shutil.move')
+		with tarfile.open(local_path+"Sauvegardedu20210526.tar") as tar:   #remplacer le sauvegardedublablabla par le argv
+			tar.extractall(local_path)
+	except tarfile.ExtractError:
+		print ('erreur')
 
 
-#condition d'utilisation
-try:
-	if sys.argv[2] == 'restore':
-		#appliqué le backup du jour etc
-	else
-		print('file does not exist')
+def restoresql():
+	try:
+		k = subprocess.Popen(["mysql -u"+ bdd_user +" -p"+ bdd_pass +" "+ bdd +" < "+ local_path+save_path+sql], shell=True)
+	except:
+		print('nope')
 
+def restorewordp():
+	original = '/home/localuser/Saves/var/www/html/'
+	target = '/var/www/'
+
+	for files in os.listdir(target):
+		i = os.path.join(target, files)
+		try:
+			shutil.rmtree(i)
+		except Exception as e:
+			print('cannot delete : ' + e)
+	try:
+		shutil.move(original, target)
+	except:
+		print('erreur shutil.move')
+
+
+def main():
+	if sys.argv[1] == 'restore':
+		extract()
+		restoresql()
+		restorewordp()
+	else:
+		print('argument inexistant où inconue')
+		exit()
