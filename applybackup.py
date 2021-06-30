f8378313a0a911c32803d3da212138be16ded21b
root <zazouimmo@gmail.com>
2021-06-30 03:02:55 +0200

    debug complet pour faire fonctioner le if sys argv = restore

--- applybackup.py
+++ applybackup.py
@@ -12,25 +12,28 @@ from scp import SCPClient
 from datetime import timedelta
 
 
-local_path = '/home/localuser/Saves/'
-sql = 'dump.sql'
-save_path = 'var/www/backupsql/'
-bdd_user = 'wordpressuser'
-bdd_pass = 'toto'
-bdd = 'wordpress'
+def extract():
+	local_path = '/home/localuser/Saves/'
 
-
-def extract(local_path):
 	try:
 		with tarfile.open(local_path+"Sauvegardedu20210526.tar") as tar:   #remplacer le sauvegardedublablabla par le argv
 			tar.extractall(local_path)
+			print('extact check')
 	except tarfile.ExtractError:
 		print ('erreur')
 
 
-def restoresql(bdd_user, bdd_pass, bdd, local_path, save_path, sql):
+def restoresql():
+	local_path = '/home/localuser/Saves/'
+	sql = 'dump.sql'
+	save_path = 'var/www/backupsql/'
+	bdd_user = 'wordpressuser'
+	bdd_pass = 'toto'
+	bdd = 'wordpress'
+
 	try:
-		k = subprocess.Popen(["mysql -u"+ (bdd_user) +" -p"+ (bdd_pass) +" "+ (bdd) +" < "+ (local_path)+(save_path+sql)], shell=True)
+		k = subprocess.Popen(["mysql -u"+ bdd_user +" -p"+ bdd_pass +" "+ bdd +" < "+ local_path+save_path+sql], shell=True)
+		print('restoresql check')
 	except:
 		print('nope')
 
@@ -42,9 +45,11 @@ def restorewordp():
 		i = os.path.join(target, files)
 		try:
 			shutil.rmtree(i)
+			print('rm check')
 		except Exception as e:
 			print('cannot delete : ' + e)
 	try:
 		shutil.move(original, target)
+		print('restorewordp check')
 	except:
 		print('erreur shutil.move')
--- testmain.py
+++ testmain.py
@@ -10,18 +10,13 @@ from scp import SCPClient
 from datetime import timedelta
 import applybackup
 
-local_path = '/home/localuser/Saves/'
-sql = 'dump.sql'
-save_path = 'var/www/backupsql/'
-bdd_user = 'wordpressuser'
-bdd_pass = 'toto'
-bdd = 'wordpress'
-
-#if sys.argv[1] = 'restore':
-applybackup.extract(local_path)
-applybackup.restoresql(local_path, save_path, bdd, bdd_user, bdd_pass, sql)
-applybackup.restorewordp()
-#else :
-#	print('argument inexistant où inconue')
-#exit(0)
-
+try:
+	if sys.argv[1] == 'restore':
+		applybackup.extract()
+		applybackup.restoresql()
+		applybackup.restorewordp()
+	else :
+		print('argument inexistant où inconue')
+	exit(0)
+except:
+	print('invalid or missing arguments')
